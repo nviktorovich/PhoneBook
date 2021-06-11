a58e2dc8170829f89679e27049422db2b0bf4199
nviktorovich <nvmaslenko@gmail.com>
2021-06-12 00:16:30 +0500

    GIT init

--- phone_book/settings.py
+++ phone_book/settings.py
@@ -11,16 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj-database-url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-
-db_from_env = dj-database-url.config()
-DATABASE['default'].update(db_from_env)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
